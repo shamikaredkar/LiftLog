98984af25f53e2c6301f385a82e90cc76a8dcb0b
Shamika Redkar <shamikaredkar91@gmail.com>
2024-07-24 12:14:08 -0400

    Project setup completed

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div>
+      <h1>IT'S TIME TO GET</h1>
+      <h1>
+        <span>Stronger</span>
+      </h1>
+      <p>
+        I <span>solemnly swear</span> to log my lifts, crush my goals, and{" "}
+        <span>never, ever skip leg day</span> (unless it's absolutely
+        necessary). I promise to <span> celebrate every PR</span> , even if it
+        means doing a victory dance in the gym. And most importantly, I vow to
+        have fun, stay motivated, and always remember that
+        <span>sweat is just my fat crying</span>. Lift on!
+      </p>
+    </div>
+  );
+}
