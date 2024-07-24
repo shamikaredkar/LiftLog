0ba89394ad1fbf0062646cc89e46cc552c2037e1
Shamika Redkar <shamikaredkar91@gmail.com>
2024-07-24 19:20:52 -0400

    Added functionality for Step 3 and enhanced features

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,6 +1,6 @@
-import React from "react";
 import SectionWrapper from "./SectionWrapper";
-import { WORKOUTS } from "../utils/workouts";
+import { SCHEMES, WORKOUTS } from "../utils/workouts";
+import React, { useState } from "react";
 
 const Header = ({ index, title, description }) => {
   return (
@@ -19,6 +19,31 @@ const Header = ({ index, title, description }) => {
 };
 
 export default function Generator() {
+  const [showmodal, setShowModal] = useState(false);
+  const [poison, setPoison] = useState("individual");
+  const [muscles, setMuscles] = useState([]);
+  const [goals, setGoals] = useState("strength_power");
+  function toggleModal() {
+    setShowModal(!showmodal); //
+  }
+  function updateMuscles(muscleGroup) {
+    if (poison !== "individual") {
+      setMuscles([muscleGroup]);
+      setShowModal(false);
+      return;
+    }
+    if (muscles.includes(muscleGroup)) {
+      setMuscles(muscles.filter((val) => val !== muscleGroup));
+    } else {
+      if (muscles.length < 3) {
+        setMuscles([...muscles, muscleGroup]);
+      }
+      if (muscles.length === 3) {
+        setShowModal(false);
+      }
+    }
+  }
+
   return (
     <SectionWrapper
       header={"Generate Your Workout"}
@@ -33,7 +58,15 @@ export default function Generator() {
         {Object.keys(WORKOUTS).map((type, index) => {
           return (
             <button
-              className=' bg-red-950 border border-red-300 py-3 rounded-lg duration-200 redShadow'
+              onClick={() => {
+                setPoison(type);
+              }}
+              className={
+                "bg-red-950 border border-red-900  py-3 rounded-lg duration-200" +
+                (type === poison
+                  ? " border-red-300 redShadow"
+                  : " border-red-900 darkRedShadow")
+              }
               style={{ backgroundColor: "#1F0021" }}
               key={index}
             >
@@ -47,14 +80,65 @@ export default function Generator() {
         title={"Target Your Muscles"}
         description={"Select the muscle groups you want to work on today."}
       />
-      <div
-        className='p-3 m-2  bg-red-950 border border-solid border-red-300 rounded-lg'
-        style={{ backgroundColor: "#1F0021" }}
-      >
-        <div className='relative flex items-center justify-center'>
-          <p>Select Muscle Groups</p>{" "}
-          <i className='fa-solid fa-caret-down absolute right-3 top-1/2 transform -translate-y-1/2 '></i>
+      <div className='flex justify-center'>
+        <div
+          className='flex flex-col m-2 bg-red-950 border border-solid border-red-300 rounded-lg w-5/12'
+          style={{ backgroundColor: "#1F0021" }}
+        >
+          <button
+            onClick={toggleModal}
+            className=' p-3 relative items-center justify-center'
+          >
+            <p>Select Muscle Groups</p>{" "}
+            <i className='fa-solid fa-caret-down absolute right-3 top-1/2 transform -translate-y-1/2 '></i>
+          </button>
+          {showmodal && (
+            <div className='flex flex-col'>
+              {(poison === "individual"
+                ? WORKOUTS.individual
+                : Object.keys(WORKOUTS[poison])
+              ).map((muscleGroup, index) => (
+                <button
+                  onClick={() => updateMuscles(muscleGroup)}
+                  className={`hover:text-red-300 hover:duration-200 hover:border hover:border-solid hover:border-white ${
+                    muscles.includes(muscleGroup) ? "text-red-300" : ""
+                  }`}
+                  key={index}
+                >
+                  <p className='uppercase p-3'>
+                    {muscleGroup.replaceAll("_", " ")}
+                  </p>
+                </button>
+              ))}
+            </div>
+          )}
         </div>
+      </div>
+      <Header
+        index={"03"}
+        title={"Define Your Goals"}
+        description={"Set your ultimate objective and kick start your journey"}
+      />
+      <div className='grid grid-cols-3 gap-4 p-4'>
+        {Object.keys(SCHEMES).map((scheme, index) => {
+          return (
+            <button
+              onClick={() => {
+                setGoals(scheme);
+              }}
+              className={
+                "bg-red-950 border border-red-900  py-3 rounded-lg duration-200" +
+                (scheme === goals
+                  ? " border-red-300 redShadow"
+                  : " border-red-900 darkRedShadow")
+              }
+              style={{ backgroundColor: "#1F0021" }}
+              key={index}
+            >
+              <p className='capitalize'>{scheme.replaceAll("_", " ")}</p>
+            </button>
+          );
+        })}
       </div>
     </SectionWrapper>
   );
