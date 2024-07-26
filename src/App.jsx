e4a4e7fa126d38000022ff4c1a8c5a0a2a50ba04
Shamika Redkar <shamikaredkar91@gmail.com>
2024-07-26 10:08:23 -0400

    Added a Navbar logo to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Generator from "./components/Generator";
 import Workout from "./components/Workout";
 import ErrorModal from "./components/ErrorModal"; // Import ErrorModal
 import { generateWorkout } from "./utils/functions";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [workout, setWorkout] = useState(null);
@@ -30,39 +31,42 @@ function App() {
   }
 
   return (
-    <main
-      style={{
-        backgroundImage: "linear-gradient(to bottom, #1F0021 50%, #751006)",
-      }}
-      className='min-h-screen flex flex-col text-[#FDFFFC] text-sm sm:text-base'
-    >
-      <Hero />
-      <Generator
-        poison={poison}
-        setPoison={setPoison}
-        muscles={muscles}
-        setMuscles={setMuscles}
-        goals={goals}
-        setGoals={setGoals}
-        updateWorkout={updateWorkout}
-      />
-      {workout && (
-        <div className='mt-32'>
-          <Workout workout={workout} />
-        </div>
-      )}
-      {showModal && (
-        <div className='fixed inset-0 flex items-center justify-center z-50'>
-          <div
-            className='bg-black opacity-50 fixed inset-0'
-            onClick={() => setShowModal(false)}
-          ></div>
-          <div className='relative z-10'>
-            <ErrorModal onClose={() => setShowModal(false)} />
+    <>
+      <Navbar />
+      <main
+        style={{
+          backgroundImage: "linear-gradient(to bottom, #1F0021 50%, #751006)",
+        }}
+        className='min-h-screen flex flex-col text-[#FDFFFC] text-sm sm:text-base'
+      >
+        <Hero />
+        <Generator
+          poison={poison}
+          setPoison={setPoison}
+          muscles={muscles}
+          setMuscles={setMuscles}
+          goals={goals}
+          setGoals={setGoals}
+          updateWorkout={updateWorkout}
+        />
+        {workout && (
+          <div className='mt-32'>
+            <Workout workout={workout} />
           </div>
-        </div>
-      )}
-    </main>
+        )}
+        {showModal && (
+          <div className='fixed inset-0 flex items-center justify-center z-50'>
+            <div
+              className='bg-black opacity-50 fixed inset-0'
+              onClick={() => setShowModal(false)}
+            ></div>
+            <div className='relative z-10'>
+              <ErrorModal onClose={() => setShowModal(false)} />
+            </div>
+          </div>
+        )}
+      </main>
+    </>
   );
 }
 
