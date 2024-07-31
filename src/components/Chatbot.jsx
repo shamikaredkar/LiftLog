7146541809e963ab022854f6e075b7501b44b79d
Shamika Redkar <shamikaredkar91@gmail.com>
2024-07-31 12:48:47 -0400

    Chatbot dismiss on click on screen

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -16,6 +16,7 @@ export default function Chatbot() {
   const [isOpen, setIsOpen] = useState(false);
   const [loading, setLoading] = useState(false);
   const chatContainerRef = useRef(null);
+  const chatbotRef = useRef(null); // Create a ref for the chatbot container
 
   const surpriseOptions = [
     "What is my workout plan for today?",
@@ -109,85 +110,111 @@ export default function Chatbot() {
     }
   }, [chatHistory]);
 
+  // Close chatbot when clicking outside of it
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (chatbotRef.current && !chatbotRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [chatbotRef, isOpen]);
+
   return (
     <div className='px-6 fixed bottom-5 right-5 z-50'>
-      <button className='rounded-full w-14 h-14 shadow-lg' onClick={toggleChat}>
-        <Lottie animationData={chatbotAnimation} loop={true} autoplay={true} />
-      </button>
-      {isOpen && (
-        <div className='bg-white border border-gray-300 rounded-lg w-[600px] h-[700px] shadow-lg flex flex-col absolute bottom-20 right-0'>
-          <div className='flex items-center pt-4 pb-1 '>
-            <div className='ml-4 flex items-center'>
-              <span className='font-bold text-lg'>GymBro</span>
+      <div ref={chatbotRef}>
+        <button
+          className='rounded-full w-14 h-14 shadow-lg'
+          onClick={toggleChat}
+        >
+          <Lottie
+            animationData={chatbotAnimation}
+            loop={true}
+            autoplay={true}
+          />
+        </button>
+        {isOpen && (
+          <div className='bg-white border border-gray-300 rounded-lg w-[600px] h-[700px] shadow-lg flex flex-col absolute bottom-20 right-0'>
+            <div className='flex items-center pt-4 pb-1 '>
+              <div className='ml-4 flex items-center'>
+                <span className='font-bold text-lg'>GymBro</span>
+              </div>
             </div>
-          </div>
-          <div className='flex-1 flex flex-col overflow-hidden'>
-            <div className='flex-1 overflow-y-auto p-4' ref={chatContainerRef}>
-              {chatHistory.map((chatItem, index) => {
-                const isLoading = chatItem.parts.includes("loader");
-                return (
-                  <div
+            <div className='flex-1 flex flex-col overflow-hidden'>
+              <div
+                className='flex-1 overflow-y-auto p-4'
+                ref={chatContainerRef}
+              >
+                {chatHistory.map((chatItem, index) => {
+                  const isLoading = chatItem.parts.includes("loader");
+                  return (
+                    <div
+                      key={index}
+                      className={`block max-w-max p-2 my-2 rounded-lg ${
+                        chatItem.role === "user"
+                          ? "bg-red-100 self-end text-right ml-auto"
+                          : !isLoading
+                          ? "bg-gray-100 self-start"
+                          : ""
+                      }`}
+                      dangerouslySetInnerHTML={{ __html: chatItem.parts }}
+                    ></div>
+                  );
+                })}
+              </div>
+              {error && <p className='text-red-500 mt-2 p-4'>{error}</p>}
+
+              {/* Suggestions Section */}
+              <div className='flex flex-col p-4 space-y-2 overflow-y-auto max-h-32'>
+                {surpriseOptions.map((option, index) => (
+                  <button
                     key={index}
-                    className={`block max-w-max p-2 my-2 rounded-lg ${
-                      chatItem.role === "user"
-                        ? "bg-red-100 self-end text-right ml-auto"
-                        : !isLoading
-                        ? "bg-gray-100 self-start"
-                        : ""
-                    }`}
-                    dangerouslySetInnerHTML={{ __html: chatItem.parts }}
-                  ></div>
-                );
-              })}
-            </div>
-            {error && <p className='text-red-500 mt-2 p-4'>{error}</p>}
+                    onClick={() => handleSuggestionClick(option)}
+                    className='text-left bg-gray-100 p-2 rounded-lg shadow-md hover:bg-gray-200'
+                  >
+                    {option}
+                  </button>
+                ))}
+              </div>
 
-            {/* Suggestions Section */}
-            <div className='flex flex-col p-4 space-y-2 overflow-y-auto max-h-32'>
-              {surpriseOptions.map((option, index) => (
-                <button
-                  key={index}
-                  onClick={() => handleSuggestionClick(option)}
-                  className='text-left bg-gray-100 p-2 rounded-lg shadow-md hover:bg-gray-200'
-                >
-                  {option}
-                </button>
-              ))}
-            </div>
-
-            <div className='flex p-4'>
-              <input
-                className='flex-1 p-2 border border-gray-300 rounded-lg'
-                value={value}
-                placeholder='How may I help you today?'
-                onChange={(e) => setValue(e.target.value)}
-              />
-              {!error && (
-                <button
-                  className='ml-2 px-2 text-white'
-                  onClick={getResponse}
-                  disabled={loading}
-                >
-                  <FontAwesomeIcon
-                    icon={faCircleChevronRight}
-                    style={{ color: "#751006" }}
-                    size='2xl'
-                  />
-                </button>
-              )}
-              {error && (
-                <button className='ml-2 px-2 text-white' onClick={clear}>
-                  <FontAwesomeIcon
-                    icon={faTimesCircle}
-                    style={{ color: "#751006" }}
-                    size='2xl'
-                  />
-                </button>
-              )}
+              <div className='flex p-4'>
+                <input
+                  className='flex-1 p-2 border border-gray-300 rounded-lg'
+                  value={value}
+                  placeholder='How may I help you today?'
+                  onChange={(e) => setValue(e.target.value)}
+                />
+                {!error && (
+                  <button
+                    className='ml-2 px-2 text-white'
+                    onClick={getResponse}
+                    disabled={loading}
+                  >
+                    <FontAwesomeIcon
+                      icon={faCircleChevronRight}
+                      style={{ color: "#751006" }}
+                      size='2xl'
+                    />
+                  </button>
+                )}
+                {error && (
+                  <button className='ml-2 px-2 text-white' onClick={clear}>
+                    <FontAwesomeIcon
+                      icon={faTimesCircle}
+                      style={{ color: "#751006" }}
+                      size='2xl'
+                    />
+                  </button>
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
